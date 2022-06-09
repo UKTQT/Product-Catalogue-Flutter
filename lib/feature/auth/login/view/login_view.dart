12f0b8d062a196e12f2b1e108d1e565d43218b95
UKTQT <ufukkt2699@hotmail.com>
2022-06-09 11:03:44 +0300

    login view update

--- lib/feature/auth/login/view/login_view.dart
+++ lib/feature/auth/login/view/login_view.dart
@@ -13,36 +13,50 @@ class LoginView extends StatelessWidget {
         child: Stack(
           alignment: Alignment.bottomCenter,
           children: [
-            SizedBox(
+            Container(
+              alignment: Alignment.topCenter,
+              color: const Color(0xfff9bc3b),
+              width: double.maxFinite,
               height: MediaQuery.of(context).size.height,
-              child: Container(
-                alignment: Alignment.topCenter,
-                color: const Color(0xfff9bc3b),
-                width: double.maxFinite,
-                child: Hero(
-                  tag: 'app_logo',
-                  child: Image(
-                    width: MediaQuery.of(context).size.width * 0.98,
-                    image: AssetImage(
-                      AppConstant.instance!.APP_LOGO_PATH,
-                    ),
+              child: Hero(
+                tag: 'app_logo',
+                child: Image(
+                  width: MediaQuery.of(context).size.width * 0.98,
+                  image: AssetImage(
+                    AppConstant.instance!.APP_LOGO_PATH,
                   ),
                 ),
               ),
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.5,
+            Container(
+              height: MediaQuery.of(context).size.height * 0.55,
               width: double.maxFinite,
-              child: Container(
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50),
-                        topRight: Radius.circular(50))),
+              decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50))),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.1,
+                  vertical: MediaQuery.of(context).size.height * 0.05,
+                ),
                 child: Column(
                   children: [
-                    Text('Hoşgeldiniz'),
-                    Text('Lütfen bilgileriniz ile giriş yapınız'),
+                    Row(children: [
+                      Text('Hoşgeldiniz',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(color: Colors.white))
+                    ]),
+                    Row(children: [
+                      Text('Lütfen bilgileriniz ile giriş yapınız',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleSmall!
+                              .copyWith(color: Colors.white))
+                    ]),
                   ],
                 ),
               ),
